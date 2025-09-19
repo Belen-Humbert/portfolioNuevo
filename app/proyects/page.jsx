3958feda42e90397f1dc8d0c44aedfaeea2b7a5d
Belen Humbert <belenhumbert69@gmail.com>
2025-09-19 17:55:37 -0300

    mejoras en sectios proyectos

--- app/proyects/page.jsx
+++ app/proyects/page.jsx
@@ -4,16 +4,15 @@ export default function proyect() {
   return (
     <div className="bg-stone-900 p-1 rounded-md shadow-[0px_5px_15px_rgba(0,0,0,1)]">
       <h1 className="p-2 font-bold text-[#9C667D] text-xl">Proyectos</h1>
-      <div className="flex flex-col items-center lg:flex-row lg:flex-wrap lg:ml-8">
-        <CardCarousel
-          proyectos={{
-            title: "Musep App",
-            description:
-              "Sistema administrativo para el Museo Histórico y Natural de Lavalle, desarrollado con JavaScript, Node.js, Express.js y Tailwind CSS. Permite registrar y gestionar piezas, administrar préstamos y hacer seguimiento de taxidermias realizadas en el museo.",
-            images: ["/museo1.webp", "/museo2.webp", "/museo3.webp"],
-            link: "https://github.com/Belen-Humbert/Museo-App-Nuevo.git",
-          }}
-        />
+      <div className="flex flex-col items-center justify-center lg:flex-row lg:flex-wrap lg:ml-12">        <CardCarousel
+        proyectos={{
+          title: "Musep App",
+          description:
+            "Sistema administrativo para el Museo Histórico y Natural de Lavalle, desarrollado con JavaScript, Node.js, Express.js y Tailwind CSS. Permite registrar y gestionar piezas, administrar préstamos y hacer seguimiento de taxidermias realizadas en el museo.",
+          images: ["/museo1.webp", "/museo2.webp", "/museo3.webp"],
+          link: "https://github.com/Belen-Humbert/Museo-App-Nuevo.git",
+        }}
+      />
 
         <CardCarousel
           proyectos={{
