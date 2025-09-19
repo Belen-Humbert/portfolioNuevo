8e07ce5dc7f362b8819231a0ecb249d1235e2343
Belen Humbert <belenhumbert69@gmail.com>
2025-09-19 17:11:28 -0300

    pequeño cambio en routejs

--- app/api/send-email/route.js
+++ app/api/send-email/route.js
@@ -1,9 +1,9 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req) {
   try {
+    const resend = new Resend(process.env.RESEND_API_KEY);
     const { name, email, message } = await req.json();
 
     const data = await resend.emails.send({
